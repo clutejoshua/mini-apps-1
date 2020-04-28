7768dfc748dd4033d6b3a607dcde702158fa7604
clutejoshua <57841091+clutejoshua@users.noreply.github.com>
2020-04-28 14:48:53 -0700

    Installed Dependencies and set up server

--- challenge_2/client/app.js
+++ challenge_2/client/app.js
@@ -1,2 +1,38 @@
+const app = {
+  init: function() {
+    $('#form').on('submit', app.onSubmit);
+    app.onGet();
+  },
 
+  onGet: data => {
+    $.ajax({
+      url: '/data',
+      method: 'GET',
+      success: data => {
+
+      },
+      error: () => {
+        console.log('This is an error');
+      }
+    });
+  },
+
+  onSubmit: event => {
+    event.preventDefault();
+    var data = ${'textarea'}.val();
+    console.log('data', typeof data);
+    $.ajax({
+      url: '/data',
+      method: 'POST',
+      contentType: 'application/json',
+      data: data,
+      success: () => {
+        app.onGet();
+      }
+    });
+    $('textarea').val('');
+  }
+};
+
+$(document).ready(app.init);
 
--- challenge_2/server.js
+++ challenge_2/server.js
@@ -1,2 +1,22 @@
+const express = require('express');
+const bodyParser = require('body-parser');
+const morgan = require('morgan');
+const cors = require('cors');
+const path = require('path');
 
 
+const app = express();
+const port = 4200;
+
+app.use(express.static('client'));
+app.use(bodyParser.urlencoded({extended:true}));
+
+app.use(cors());
+app.use(morgan('dev'));
+
+app.post('/server', (req, res) => {
+  console.log(req.body);
+  res.status(200).send(req);
+});
+
+app.listen(port, () => console.log(`app listening at http://local:${port}`));
