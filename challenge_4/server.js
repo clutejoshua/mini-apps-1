8b89d435bc7b293a278607004a5efebb99c3c804
clutejoshua <57841091+clutejoshua@users.noreply.github.com>
2020-04-28 16:42:36 -0700

    Set up server

--- challenge_4/server.js
+++ challenge_4/server.js
@@ -1,8 +1,17 @@
 const express = require('express');
-const app = express();
 const bodyParser = require('body-parser');
 const path = require('path');
+const cors = require('cors');
+const morgan = require('morgan');
+
 const port = 3000;
+const app = express();
+
+app.use(morgan('dev'));
+app.use(cors())
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded( {extended: true} ));
+
 
 app.use(express.static(path.join(__dirname, 'public')));
 
